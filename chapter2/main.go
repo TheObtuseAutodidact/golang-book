5c4ff0ee0855baf5205fbbf611a564a805924ae0
Obtuse <theobtuseautodidact@gmail.com>
2017-01-20 21:54:38 -0800

    A quick run through chapter 3

--- chapter2/main.go
+++ chapter2/main.go
@@ -4,6 +4,8 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, World!")
+	fmt.Println("Hello, World!"[1])
+	fmt.Println("Hello, " + "World!")
 	myName := "Lenny"
 	fmt.Println("Hello, my name is", myName)
 }
